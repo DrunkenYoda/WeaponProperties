092d76f5a2bc8db85c4eff2e94fcc727e88b293c
DrunkenYoda <82773917+DrunkenYoda@users.noreply.github.com>
2024-02-10 15:03:43 -0600

    Update heavyarmsgs.js

--- heavyarmsgs.js
+++ heavyarmsgs.js
@@ -1,8 +1,19 @@
-Hooks.on('ready', () => {
-	CONFIG.DND5E.weaponProperties['ammunition'] = 'Ammunition';
-  	CONFIG.DND5E.weaponProperties['kick'] = 'Kick';
-	CONFIG.DND5E.weaponProperties['loud'] = 'Loud';
-	CONFIG.DND5E.weaponProperties['pocket'] = 'Pocket';
-	CONFIG.DND5E.weaponProperties['reload'] = 'Reload';
-	
+Hooks.once("init", () => {
+  CONFIG.DND5E.itemProperties.kick= {
+    label: "Kick",
+    isPhysical: "true"
+  };
+  CONFIG.DND5E.validProperties.weapon.add("kick");
+
+  CONFIG.DND5E.itemProperties.loud= {
+    label: "Loud",
+    isPhysical: "true"
+  };
+  CONFIG.DND5E.validProperties.weapon.add("loud");
+
+  CONFIG.DND5E.itemProperties.pocket= {
+    label: "Pocket",
+    isPhysical: "true"
+  };
+  CONFIG.DND5E.validProperties.weapon.add("pocket");
 });
